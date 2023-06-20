45a0cf114a05ad72e5497ce7b6c745886f4dd7e4
Antonio Felix Glorioso Ceretti <antonioglorioso1@hotmail.com>
2023-06-19 22:02:52 -0300

    Donaciones
    Agregado sector de donaciones

--- src/app/servicios/campanias.service.ts
+++ src/app/servicios/campanias.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { getDB } from './get-db.service';
 import { Observable } from 'rxjs';
+import { Campania } from '../interfaces/Campania';
 
 const httpOptions = {
 	headers: new HttpHeaders({
@@ -23,5 +24,15 @@ export class CampaniasService extends getDB {
 	public getByTitulo(titulo: string): Observable<any> {
 		const url = `${this.apiUrl}/getByTitulo/${titulo}`
 		return this.http.get<any>(url)
+	}
+
+	public selectCampania(campania: Campania): Observable<any> {
+		const url = `${this.apiUrl}/seleccionarCampaña/${campania.id}`
+		return this.http.get<any>(url);
+	}
+
+	public getCampaniaSeleccionada(): Observable<any> {
+		const url = `${this.apiUrl}/getCampaniaSeleccionada`
+		return this.http.get<any>(url);
 	}
 }
